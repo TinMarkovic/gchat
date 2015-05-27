494b0a9d8f5c618f29c935f2a3753b5181821c5e
root <root@vagrant-centos65.vagrantup.com>
2015-05-27 10:56:38 +0000

    del

--- dbInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//bez dodataka samo info baze
-require_once '/etc/gchat/db_config.php';
-require_once 'db.php';
-new DB($dbCfg['host'], $dbCfg['username'], $dbCfg['password'], $dbCfg['db']);
-
-?>
